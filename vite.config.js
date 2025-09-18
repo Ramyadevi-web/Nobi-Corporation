14b78630ab125fc7627e089f6280cd926b4f6bc3
Ramyadevi-web <ramyadevim7@gmail.com>
2025-09-18 11:51:03 +0530

    Fix 504 error at react/drei

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
-});
+  optimizeDeps: {
+    // make sure vite pre-bundles these heavy deps
+    include: ['framer-motion', '@react-three/drei'],
+    // but don't touch stats.js (we don't use it)
+    exclude: ['stats.js'],
+    force: true
+  },
+  build: {
+    rollupOptions: {
+      // keep stats.js out of the bundle
+      external: ['stats.js'],
+    },
+  },
+  resolve: {
+    alias: {
+      // if something still tries to import stats.js, point it to an empty shim
+      'stats.js': '/src/shims/stats.js',
+    },
+  },
+})
