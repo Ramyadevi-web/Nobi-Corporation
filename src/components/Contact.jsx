072dbe18edbcb2cb4142407e030de2d66271099a
Ramyadevi-web <ramyadevim7@gmail.com>
2025-09-23 00:12:07 +0530

    Fetch post added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,10 +50,17 @@ function Contact() {
 
     if (!validateEmail(email)) return alert('Please enter a valid email.');
     if (!validatePhone(phone)) return alert('Please enter a valid 10-digit phone number.');
-    alert('Form submitted successsfully');
-    console.log(Object.fromEntries(formData.entries()))
-
-    form.reset();
+    
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type':'applicaiton/x-www-form-urlencoded'},
+      body:new URLSearchParams(formData).toString()
+    }
+    ).then(() =>{
+      alert("Form submitted sucessfully");
+      form.reset();
+    })
+    .catch((error)=> alert(error))
   };
 
   const inputClasses = 'mb-3 p-2 border border-gray bg-transparent text-black placeholder-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-[#20576E]';
@@ -62,7 +69,7 @@ function Contact() {
     <section id='contact' className='flex flex-col md:flex-row items-stretch gap-10 justify-center bg-radial-ellipse-even py-10 px-5'>
       
       {/* Service Form */}
-      <form ref={serviceFormRef} name='contact' data-netlify='true' method = 'POST' className='flex flex-col bg-transparent rounded p-6 w-full md:w-1/2'>
+      <form ref={serviceFormRef} name='contact' data-netlify='true' method = 'POST' className='flex flex-col bg-transparent rounded p-6 w-full md:w-1/2' onSubmit={handleServiceSubmit}>
         <h3 className='text-2xl font-semibold mb-4 text-[#20576E] text-center'>Request Our Service</h3>
         <input type="text" name="name" placeholder="Full Name" required className={inputClasses} />
         <input type='hidden' name='form-name' value='contact'/>
